aba9f8b167c38f0e9af9cff4640cca7a1192be39
johnjou7 <72895294+johnjou7@users.noreply.github.com>
2023-07-03 13:38:51 -0400

    hice un cambio en el index de la main para ver si así funciona la página

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-  echo 'hola baby';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PROY ONE</title>
+</head>
+<body>
+  HOLA
+</body>
+</html>
