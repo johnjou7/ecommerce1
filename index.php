340e23a84e14b5d480026bb090b4f94f57e0a022
johnjou7 <72895294+johnjou7@users.noreply.github.com>
2023-07-08 17:58:30 -0400

    solo estoy tratando de aprender algunas cosas

--- index.php
+++ index.php
@@ -43,3 +43,15 @@
     <?php endif; ?>
   </body>
 </html>
+
+
+/* 
+3. Esta línea inicia una sesión en PHP. Las sesiones se utilizan para almacenar y acceder a datos específicos del usuario a través de múltiples páginas.
+
+4. Esta línea incluye el archivo "database.php" en el código actual. Esto se utiliza para establecer una conexión con la base de datos y realizar consultas.
+
+6. isset  es una función en PHP que se utiliza para verificar si una variable está definida y no es nula. Devuelve un valor booleano, es decir,  true  si la variable está definida y no es nula, y  false  si la variable no está definida o es nula. 
+La variable superglobal  $_SESSION  sería como una libreta que el servidor utiliza para mantener un registro de la información asociada con cada sesión. En esta libreta, el servidor guarda tu nombre y cualquier otra información relevante que necesites mantener durante la conversación.
+
+7. 
+*/
