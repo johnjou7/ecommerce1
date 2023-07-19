64e6718d08505ce78d1ebafb6921197f20726440
johnjou7 <72895294+johnjou7@users.noreply.github.com>
2023-07-19 14:45:27 -0400

    estuve trabajando en el maquetado de la interfaz grafica del login y además cree el archivo style.css, que es para estilos globales

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
   session_start();
   require('APIs/consultas_db.php');
 
+  // si ya existe una sesión, entonces se pasa directamente a la interface home que le corresponde.
   if(isset($_SESSION['id_user'])){
     header('Location: modules/login-register/interface-usershome.php');
     exit();
   }
+  // si aún no existe ninguna sesión, entonces se pasa a la interface de login.
   else{
     header('Location: modules/login-register/interface-login.php');
     exit();
--- modules/login-register/interface-login.php
+++ modules/login-register/interface-login.php
@@ -1,1 +1,19 @@
-ESTA ES LA INTERFAZ DE LOGIN
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="../../assets/css/style.css">
+</head>
+<body>
+  <div class="ventana-login">
+    <h1>Login</h1>
+    <div class="contenedor-in-login">
+      <input name="nombre" type="text" placeholder="Ingresa tu nombre aquí">
+      <input name="contrasena" type="password" placeholder="Ingresa tu contraseña aquí">
+      <input name="entrar-button" type="submit" value="Entrar">
+    </div>
+  </div>
+</body>
+</html>
